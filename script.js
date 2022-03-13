b4132a4c00927705eb24ebc39cd3e1480f3c61f7
matekem <mate.kemenesi@gmail.com>
2022-03-13 14:19:42 +0100

    Added some styling

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container')
+const btnPicker = document.createElement('button')
 const btnBlack = document.createElement('button')
 const btnRgb = document.createElement('button')
 const btnGray = document.createElement('button')
@@ -9,7 +10,7 @@ const buttonsContainer = document.querySelector('.buttons');
 function createDivs(col,rows){
     for(let i = 0; i < (col*rows); i++ ){
         const div = document.createElement('div');
-        div.style.border = "0.1px solid lightgray";
+        
         container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
         container.appendChild(div).classList.add('box');
@@ -70,10 +71,10 @@ function reSet(){
     boxs.forEach(box => box.remove())
 }
 function reSize(){
-    btnSize.textContent = 'GRID SIZE'
+    btnSize.textContent = 'RESIZE'
     btnSize.addEventListener('click', () => {
         let user = prompt('What size grid would you like to create?')
-        if(user === null || user < 1){
+        if(user === null || user < 1 || user >   64){
             reSet();
             createDivs(16,16)
             blackColor()
@@ -91,3 +92,18 @@ function reSize(){
     buttonsContainer.appendChild(btnSize).classList.add('btn')
 }
 reSize();
+
+function colorPicker() { 
+    
+    const boxs = container.querySelectorAll('.box')
+    btnPicker.textContent ="Pick a color"
+    btnPicker.addEventListener('click', function() {
+        boxs.forEach(box => box.addEventListener('mouseover',function() {
+            let R = Math.floor(Math.random() * 255)
+            let G = Math.floor(Math.random() * 255)
+            let B = Math.floor(Math.random() * 255)
+            this.style.background = `rgb(${R},${G},${B})`;
+        }))
+    })
+    buttonsContainer.appendChild(btnRgb).classList.add('btn');
+}
